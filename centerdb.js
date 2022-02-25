f79a1e54e8e273a3d92a6d17a8ec4af98ab8ebf6
DIBYANSHU JHA <85605269+dibyanshujha12345@users.noreply.github.com>
2022-02-25 13:17:11 +0530

    Add files via upload

---
+++ centerdb.js
@@ -0,0 +1,11 @@
+const centers=[
+    {
+        id:1,
+        img:"https://cdn.wccftech.com/wp-content/uploads/2016/09/spacee-2060x1288.jpg",
+        name:"Space Starer"
+    },
+    {
+        id:2,
+
+    }
+]
---
+++ screens/TopicScreen.js
@@ -0,0 +1,14 @@
+import React, * as react from 'react';
+import {Text,View} from 'react-native';
+
+export default class TopicScreen extends React.Component{
+    render(){
+        return(
+        <View styles={{flex:1,backgroundColor:"white"}}>
+            <Text styles={{color:"black"}}>Topic Screen</Text>
+
+        </View>
+
+        )
+    }
+}
